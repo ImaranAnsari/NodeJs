807f8a1ad67fe98b19a3ce00d7c0ef368181bd6d
Imaran Ansari <i.ansarimca@gmail.com>
2022-04-05 13:46:15 +0530

    ch
    start day

--- workspace/app.js
+++ workspace/app.js
@@ -5,7 +5,22 @@ const app = express();
 app.get('/',(req, res)=>{
 
     console.log("here");
-
+    res.send("I am Respons");
 
 });
+ app.get('/add', (req, res)=>{
+
+    console.log("Application");
+     let n1=6;
+     let n2= 8;
+     let sum=n1+n2;
+     res.send("sum  = ", sum);//  express deprecated res.send(body, status): Use res.status(status).send(body)
+     res.status("sum",sum);
+ })
+
+//  express deprecated res.send(body, status): Use res.status(status).send(body)
+
+// var msg =  require('./Message.js');
+// console.log(msg);
+
 app.listen(3000);
---
+++ workspace/obj.js
@@ -0,0 +1,13 @@
+// var obj = {
+//     authorName: 'Ryan Dahl',
+//     language: 'Node.js'
+// }
+// ///console.log("object is ",obj);
+// console.log(obj.authorName);
+
+function Display(x) { 
+    x=6;
+    console.log(x);
+}
+
+Display();
