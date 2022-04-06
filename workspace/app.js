42fc5874c042f78143f5cff15983fab3e9a82675
Imaran Ansari <i.ansarimca@gmail.com>
2022-04-06 10:09:12 +0530

    gitIgnore
    for ignore module

--- workspace/app.js
+++ workspace/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get('/',(req, res)=>{
 });
  app.get('/add', (req, res)=>{
 
-    console.log("Application");
+     //console.log("Application");
      let n1=6;
      let n2= 8;
      let sum=n1+n2;
@@ -18,6 +19,17 @@ app.get('/',(req, res)=>{
      res.status("sum",sum);
  })
 
+
+app.post('/add',(req, res)=>{
+    
+    console.log(req.body);
+    // let n1=4;
+    // let n2= 9;
+    let sum=n1+n2;
+    //let sum=0;
+     res.send("sum  = ", sum);
+
+})
 //  express deprecated res.send(body, status): Use res.status(status).send(body)
 
 // var msg =  require('./Message.js');
