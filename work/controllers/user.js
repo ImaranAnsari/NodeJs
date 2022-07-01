e221b2288edc616816e32b5175579bdbb05346ed
ImaranAnsari <i.ansarimca@gmail.com>
2022-07-01 17:40:03 +0530

    test

--- work/controllers/user.js
+++ work/controllers/user.js
@@ -1,26 +1,44 @@
-const userModel = require(`../models/user`);
-
+const userModel = require('../models/user');
 
 //register
-module.exports.register = (req, res) =>{
-    userModel.insert(req, res);
-    res.send(req.body);
+module.exports.register = async (req, res) => {
+    let insertedId = await userModel.insert(req.body);
+    if(insertedId > 0){
+        res.send({status:"success", data:{id:insertedId}});
+    } else {
+        res.send({status:"error", message:"User registration failed"});
+    }
 }
 
-//get 
-module.exports.get = (req, res) =>{
+//get
+module.exports.get = async (req, res) => {
 
-    res.send(req.body);
+    let data = await userModel.get(req.body);
+    if(data){
+        res.send({status:"success", data:data});
+    } else {
+        res.send({status:"error", message:"User not found"});
+    }
 }
 
 //update
-module.exports.update = (req, res) =>{
+module.exports.update = async (req, res) => {
 
-    res.send(req.body);
+    let status = await userModel.update(req.body);
+    if(status){
+        res.send({status:"success", message:"User data updated"});
+    } else {
+        res.send({status:"error", message:"User not updated"});
+    }
 }
 
 //delete
-module.exports.delete = (req, res) =>{
-
-    res.send(req.body);
+module.exports.delete = async (req, res) => {
+    
+    let status = await userModel.delete(req.body);
+    if(status){
+        res.send({status:"success", message:"User deleted!"});
+    } else {
+        res.send({status:"error", message:"User not deleted"});
+    }
 }
